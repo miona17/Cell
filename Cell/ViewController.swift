5ef8968dc8808a866e716effc876923d03619e43
倉茂未央那 <miona2847@softbank.ne.jp>
2018-05-12 20:12:00 +0900

    カレンダー mannji

--- Cell/ViewController.swift
+++ Cell/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var testLabel: UILabel!
+}
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return 30
+    }
+    
+    
+    //データを返すメソッド
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+        //コレクションビューから識別子「TestCell」のセルを取得する。
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("TestCell", forIndexPath: indexPath) as! TestCollectionViewCell
+        
+        //セルの背景色をランダムに設定する。
+        cell.backgroundColor = UIColor(red: CGFloat(drand48()),
+                                       green: CGFloat(drand48()),
+                                       blue: CGFloat(drand48()),
+                                       alpha: 1.0)
+        
+        //セルのラベルに番号を設定する。
+        cell.testLabel.text = String(indexPath.row + 1)
+        
+        return cell
+        
+    }
 
-    override func viewDidLoad() {
+func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
-}
+
 
